285b31ccf949ba3a85a32a48229b5febdc00c49d
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2014-03-30 21:11:07 -0400

    Rework to a more general solution

--- loader/preprocess.js
+++ loader/preprocess.js
@@ -1,31 +1,53 @@
 var preprocess = (function(){
 
+
+
+  /**
+   * DOMParser HTML extension
+   * 2012-09-04, https://gist.github.com/1129031
+   *
+   * By Eli Grey, http://eligrey.com
+   * Public domain.
+   * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+   */
+  (function(DOMParser) {
+    "use strict";
+
+    var
+    DOMParser_proto = DOMParser.prototype
+    , real_parseFromString = DOMParser_proto.parseFromString
+    ;
+
+    // Firefox/Opera/IE throw errors on unsupported types
+    try {
+      // WebKit returns null on unsupported types
+      if ((new DOMParser).parseFromString("", "text/html")) {
+        // text/html parsing is natively supported
+      return;
+      }
+    } catch (ex) {}
+
+    DOMParser_proto.parseFromString = function(markup, type) {
+      if (/^\s*text\/html\s*(?:;|$)/i.test(type)) {
+        var
+        doc = document.implementation.createHTMLDocument("")
+        ;
+        if (markup.toLowerCase().indexOf('<!doctype') > -1) {
+          doc.documentElement.innerHTML = markup;
+        }
+        else {
+          doc.body.innerHTML = markup;
+        }
+        return doc;
+      } else {
+        return real_parseFromString.apply(this, arguments);
+      }
+    };
+  }(DOMParser));
+
+
+
   var Path = Filer.Path;
-
-  /**
-   * Look for <link> hrefs, e.g.
-   *
-   * <link rel="stylesheet" href="http://yandex.st/highlightjs/7.3/styles/github.min.css">
-   * <link rel="stylesheet" href="compiled/demo.css">
-   */
-  var linkRegex = /(\<link[^>]+href\s*=\s*['"])([^'"]+)(['"])/g;
-
-  /**
-   * Look for <script>s
-   *
-   * <script src="http://imagesloaded.desandro.com/imagesloaded.pkgd.min.js"></script>
-   * <script src="js/styleguide.js"></script>
-   */
-  var scriptRegex = /(\<script[^>]+src\s*=\s*['"])([^'"]+)(['"])/g;
-
-  /**
-   * Look for CSS url(...)
-   *
-   * background: url(http://foo.com/image.png)
-   * background: url('http://foo.com/image.png')
-   * background: url("../image.png")
-   */
-  var urlRegex = /(url\s*\(['"]?)([^)]+)(['"]?\))/g;
 
   function toDataURL(data, type) {
     // css - data:text/css;base64,
@@ -44,61 +66,66 @@ var preprocess = (function(){
   function html(unprocessed, location, fs) {
     location = Path.join('/', location);
     var dir = Path.dirname(location);
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(unprocessed, 'text/html');
 
-    function processCSS(match, p1, p2, p3, offset, string) {
-      if(/^\s*(http|https)?\:?\/\//.test(p2)) {
-        return match;
+    function elements(type, urlType, mime, callback) {
+      var elems = doc.querySelectorAll(type);
+      var count = 1;
+      function cb(err) {
+        console.log('err', err, count, elems.length);
+        count++;
+        if(count === elems.length - 1) {
+          callback();
+        }
       }
 
-      var cssFilename = Path.resolve(dir, p2);
-      fs.readFile(cssFilename, 'utf8', function(err, data) {
-        if(err) return match;
-        return toDataURL(data, 'text/css');
+      async.eachSeries(elems, function(elem, cb) {
+        // Skip any links for protocols (we only want relative paths)
+        var url = elem.getAttribute(urlType);
+        if(!url || /\/\//.test(url)) {
+          return cb();
+        }
+
+        var path = Path.resolve(dir, url);
+        fs.exists(path, function(found) {
+          if(!found) {
+            return cb();
+          }
+          fs.readFile(path, 'utf8', function(err, data) {
+            if(err) return cb(err);
+            elem[urlType] = toDataURL(data, mime);
+            cb();
+          });
+        });
+      }, function(err) {
+        if(err) {
+          console.log('err', err);
+        }
+        callback();
       });
     }
 
+    // Replace links with file contents from fs
+    async.series([
+      function links(callback) {
+        elements('link', 'href', 'text/css', callback);
+      },
+      function scripts(callback) {
+        elements('script', 'src', 'text/javascript', callback);
+      }
+    ], function callback(err, result) {
+      document.write(doc.documentElement.innerHTML);
+    });
   }
 
   function css(unprocessed, location, fs) {
-
+    // TODO
   }
 
-  // XXXhack - something quick and dirty to prove the point
-  // in the makerstrap demo/index.html context.  We need to deal with just these
-  // cases:
-  // <link rel="stylesheet" href="compiled/demo.css">
-  // <script src="js/vendor/angular-highlightjs.min.js"></script>
-  // <script src="js/styleguide.js"></script>
-  return {
-    html: function(unprocessed, location, fs, callback) {
-      fs.readFile('/makerstrap-master/demo/compiled/demo.css', 'utf8', function(err, data) {
-        if(err) return callback(err);
-        var processed = unprocessed.replace('compiled/demo.css', toDataURL(data, 'text/css'));
-
-        fs.readFile('/makerstrap-master/demo/js/vendor/angular-highlightjs.min.js', 'utf8', function(err, data) {
-          if(err) return callback(err);
-          processed = processed.replace('js/vendor/angular-highlightjs.min.js', toDataURL(data, 'text/javascript'));
-
-          fs.readFile('/makerstrap-master/demo/js/styleguide.js', 'utf8', function(err, data) {
-            if(err) return callback(err);
-            processed = processed.replace('js/styleguide.js', toDataURL(data, 'text/javascript'));
-console.log(processed);
-            callback(null, processed);
-          });
-        });
-      });
-    },
-
-    css: function(unprocessed, location, fs, callback) {
-
-    }
-  };
-
-/**
   return {
     html: html,
     css: css
   };
-**/
 
 }());
