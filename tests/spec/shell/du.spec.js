8a0c8f24b0da9b2a0b6e0b2b677233a740434385
Shuming Lin <kevin.shuminglin@gmail.com>
2014-09-26 23:29:24 -0400

    remove un-used commend code in du.spec.js file

--- tests/spec/shell/du.spec.js
+++ tests/spec/shell/du.spec.js
@@ -41,18 +41,12 @@ describe('FileSystemShell.du', function() {
 
           var item0 = list[0];
           expect(item0.path).to.equal('file');
-          //expect(item0.links).to.equal(1);
           expect(item0.size).to.equal(1);
-          //expect(item0.modified).to.be.a('number');
-          //expect(item0.type).to.equal('FILE');
           expect(item0.contents).not.to.exist;
 
           var item1 = list[1];
           expect(item1.path).to.equal('file2');
-          //expect(item1.links).to.equal(1);
           expect(item1.size).to.equal(2);
-          //expect(item1.modified).to.be.a('number');
-          //expect(item1.type).to.equal('FILE');
           expect(item0.contents).not.to.exist;
 
           done();
@@ -89,26 +83,17 @@ describe('FileSystemShell.du', function() {
                 list.forEach(function(item, i, arr) {
                   switch(item.path) {
                   case 'dir2':
-                    //expect(item.links).to.equal(1);
                     expect(item.size).to.be.a('number');
-                    //expect(item.modified).to.be.a('number');
-                    //expect(item.type).to.equal('DIRECTORY');
                     expect(item.contents).to.exist;
                     expect(item.contents.length).to.equal(1);
                     var contents0 = item.contents[0];
                     expect(contents0.path).to.equal('file');
-                    //expect(contents0.links).to.equal(1);
                     expect(contents0.size).to.equal(1);
-                    //expect(contents0.modified).to.be.a('number');
-                    //expect(contents0.type).to.equal('FILE');
                     expect(contents0.contents).not.to.exist;
                     break;
                   case 'file':
                   case 'file2':
-                    //expect(item.links).to.equal(1);
                     expect(item.size).to.equal(1);
-                    //expect(item.modified).to.be.a('number');
-                    //expect(item.type).to.equal('FILE');
                     expect(item.contents).not.to.exist;
                     break;
                   default:
