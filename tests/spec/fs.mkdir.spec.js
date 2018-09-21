f58da8fdb72f1f3794bdbcb03e9de681b0fb33c2
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2018-09-21 15:06:06 -0400

    Add tests for mkdir with new options

--- tests/spec/fs.mkdir.spec.js
+++ tests/spec/fs.mkdir.spec.js
@@ -20,6 +20,69 @@ describe('fs.mkdir', function() {
     });
   });
 
+  it('should create a dir if passed a mode only (recursive:false default) and path does not include a parent', function(done) {
+    var fs = util.fs();
+
+    fs.mkdir('/tmp', '777', function(error) {
+      expect(error).not.to.exist;
+
+      fs.stat('/tmp', function(error, stats) {
+        expect(error).not.to.exist;
+        expect(stats).to.exist;
+        expect(stats.type).to.equal('DIRECTORY');
+
+        done();
+      });
+    });
+  });
+
+  it('should return an error if passed a mode only (recursive:false default) and path includes parent that does not exist', function(done) {
+    var fs = util.fs();
+
+    fs.mkdir('/tmp/mydir', '777', function(error) {
+      expect(error).to.exist;
+      expect(error.code).to.equal('ENOENT');
+      done();
+    });
+  });
+
+  it('(promises) should return an error if passed a mode only (recursive:false default) and path includes parent that does not exist', function() {
+    var fsPromises = util.fs().promises;
+
+    return fsPromises
+      .mkdir('/tmp/mydir', '777')
+      .then(() => {
+        expect.fail('This should not happen');
+      })
+      .catch(function(error) {
+        expect(error).to.exist;
+        expect(error.code).to.equal('ENOENT');  
+      });
+  });
+
+
+
+  it('should create parent dir parts if recursive:true is set options', function(done) {
+    var fs = util.fs();
+
+    fs.mkdir('/tmp/mydir', {recursive: true}, function(error) {
+      expect(error).not.to.exist;
+
+      fs.stat('/tmp', function(error, stats) {
+        expect(error).not.to.exist;
+        expect(stats).to.exist;
+        expect(stats.type).to.equal('DIRECTORY');
+
+        fs.stat('/tmp/mydir', function(error, stats) {
+          expect(error).not.to.exist;
+          expect(stats).to.exist;
+          expect(stats.type).to.equal('DIRECTORY');
+          done();
+        });
+      });
+    });
+  });
+
   it('should return an error if the path already exists', function(done) {
     var fs = util.fs();
 
